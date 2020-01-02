9f9831efd648db39de055f0b81a5954f65c8c120
stirofrancesco <40853631+stirofrancesco@users.noreply.github.com>
2020-01-02 16:53:34 +0100

    Update!
    Creazione grafo di correlazione e preparazione della metapathway per il confronto

--- progettodm.R
+++ progettodm.R
@@ -1,138 +1,145 @@
 library(igraph)
-library(MASS)
-library(Matrix)
+library(matrix)
 library(DNetFinder)
-library(matrixStats)
+library(stringi)
 
-#Lettura Matrice di espressione e creazione del grafo di correlazione
+#Creazione del grafo di correlazione
 
+
+#Lettura dataset di espressione
 expression <- read.delim("expression_data.txt",
                           header = TRUE,
                           sep = "\t",
                           colClasses = "character")
 
-
-matrix <- data.matrix(expression)
-matrix <- matrix[1:1000,1:100]
-matrix <- t(matrix)
-matrix <- matrix[,apply(matrix,2,sd)!=0]
-coefGGM <- lassoGGM(matrix)
-
+#Caricamento nodi METAPATHWAY
+nodes<- read.delim("nodes.txt",
+                   header = TRUE,
+                   sep = "\t",
+                   colClasses = "character")
 
 
+#Caricamento archi METAPATHWAY
+edges<- read.delim("edges.txt",
+                   header = TRUE,
+                   sep = "\t",
+                   colClasses = "character")
+
+
+#Tengo solamente Attivazione e Inibizione e assegno i pesi negli archi della METAPATHWAY.
+{
+edges$Type <- NULL
+edges <- edges[!(edges$Subtype!="ACTIVATION" & edges$Subtype!="INHIBITION"),]
+edges$Subtype[edges$Subtype=="ACTIVATION"] <- "1"
+edges$Subtype[edges$Subtype=="INHIBITION"] <- "-1" 
+edges$Subtype = as.numeric(as.character(edges$Subtype))
+colnames(edges)[3] <- "weight"
+}
+
+#Considero solo i geni presenti sia nella METAPATHWAY sia nel dataset di espressione.
+{
+  
+  v1 <- rownames(expression)
+  v2<- unique(c(edges$X.Start,edges$End))
+  
+  common <- c()
+  for(i in 1:length(v1)){
+    if(v1[i] %in% v2) common <- c(common,v1[i])
+  }
+  
+  expression <- expression[common,]
+  edges <- edges[(edges$X.Start %in% common) & (edges$End %in% common),]
+  
+  v1 <- rownames(expression)
+  v2<- unique(c(edges$X.Start,edges$End))
+  
+  common <- c()
+  for(i in 1:length(v1)){
+    if(v1[i] %in% v2) common <- c(common,v1[i])
+  }
+  
+  expression <- expression[common,]
+}
+
+#Creazione grafo METAPATHWAY
+metapathway <- graph_from_data_frame(edges,
+                                     directed=TRUE)
+
+#Creazione matrice di adiacenza METAPATHWAY
+adj_metapathway <- as.matrix(as_adjacency_matrix(metapathway))
+
+
+# CREAZIONE DEL GRAFO DI CORRELAZIONE
+
+#Sampling delle matrici
+{
+  numrow <- 300
+  numcol <- 300
+}
+
+#Carico la matrice di espressione dal dataset
+expression_matrix <- data.matrix(expression)
+
+# Calcolo della trasposta della matrice di espressione
+expression_matrix <- t(expression_matrix)
+
+#Ordino i geni sulla base della metapathway
+expression_matrix <- expression_matrix[,rownames(adj_metapathway)]
+
+#Riduco numero di righe e colonne
+{
+  expression_matrix <- expression_matrix[1:numrow,1:numcol]
+  adj_metapathway <- adj_metapathway[1:numrow,1:numcol]
+}
+
+#Calcolo i coefficenti di regressione (scegliere tra lassoGGM() e lassoNPN() )
+coefGGM <- lassoNPN(expression_matrix)
+
+#Setto a 0 le colonne in cui lasso non ha restituito un valore.
+coefGGM[is.na(coefGGM)] <- 0
+
+#Creo la matrice di adiacenza, inizialmente con tutti 0.
+precision_matrix <- matrix(0,nrow = numcol, ncol = numcol)
+
+#Introduco nuovamente i nomi dei geni
+{
+rownames(precision_matrix) <- colnames(expression_matrix)
+colnames(precision_matrix) <- colnames(expression_matrix)
+}
+
+#Riempimento della matrice di precisione.
+for(i in 1:numcol){
+  for(j in 1:numcol){
+    if(i<j){
+      if(coefGGM[i,j]>0) precision_matrix[i,j] = 1
+      else if(coefGGM[i,j]<0) precision_matrix[i,j] = -1
+    }
+    else if(i>j){
+      if(coefGGM[i-1,j]>0) precision_matrix[i,j] = 1
+      else if(coefGGM[i-1,j]<0) precision_matrix[i,j] = -1
+    }  
+  }
+}
+
+for(i in 1:numcol-1)
+  for(j in 1:numcol){
+    is.na(coefGGM[i,j])==TRUE
+  }
+
+#Creazione del grafo di correlazione
+correlation_graph <- graph_from_adjacency_matrix(precision_matrix, 
+                                                  mode = "directed",
+                                                  weighted = TRUE
+                                                )
+
+#Stampa del grafo ci correlazione
+plot(correlation_graph)
 
 
 
+#Confronto con la METAPATHWAY (Non finito)
 
 
 
-
-
-# 
-# 
-# vector <- colSds(matrix)
-# 
-# 
-# 
-# expression<-t(expression)
-# expression <- expression[,1:800]
-# expression[] <- lapply(expression, as.numeric)
-# 
-# 
-# 
-# 
-# 
-# 
-# nodes<- read.delim("nodes.txt",
-#                    header = TRUE,
-#                    sep = "\t",
-#                    colClasses = "character")
-# 
-# 
-# 
-# 
-# clinical <- read.delim("clinical_data.txt",
-#                        header = TRUE,
-#                        sep = "\t",
-#                        colClasses = "character")
-# 
-# edges<- read.delim("edges.txt",
-#                    header = TRUE,
-#                    sep = "\t",
-#                    colClasses = "character")
-# 
-# 
-# 
-# #Preparo la matrice
-# 
-# edges$Type <- NULL
-# edges <- edges[!(edges$Subtype!="ACTIVATION" & edges$Subtype!="INHIBITION"),]
-# as.character(edges$Subtype)
-# edges$Subtype[edges$Subtype=="ACTIVATION"] <- "1"
-# edges$Subtype[edges$Subtype=="INHIBITION"] <- "-1"
-# edges$Subtype = as.numeric(as.character(edges$Subtype))
-# colnames(edges)[3] <- "weight"
-# 
-# 
-# #ed = data.frame(Genes=unique(as.character(unlist(edges[, c("X.Start", "End")])))); 
-# #nodesNew <- merge(ed, nodes, by = c(nodes[,1]), all=TRUE) MERGE DI DUE DATASET
-# 
-# 
-# graph <- graph_from_data_frame(edges, directed=TRUE)
-# #simplify(graph, remove.multiple = TRUE, remove.loops = TRUE,
-# #         edge.attr.comb = igraph_opt("edge.attr.comb"))
-# 
-# precision_matrix <- as_adjacency_matrix(graph, 
-#                                         names = TRUE,
-#                                         attr = "weight", 
-#                                         sparse=T)
-# 
-# precision_matrix <- precision_matrix[2100:2150, 2100:2150]
-# 
-# write.matrix(precision_matrix,
-#              file="matrix.txt",
-#              sep = "\t")
-# 
-# matrix <- read.delim("matrix.txt",
-#                      header = TRUE,
-#                      sep = "\t")
-# 
-# 
-# #CALCOLO DELLA PRECISION MATRIX
-# 
-# n <- nrow(precision_matrix) # Numero di vertici, inoltre numero righe = numero colonne nella precision_matrix
-# 
-# 
-# for(k in 1:n){
-#   for(i in 1:n){
-#     for(j in 1:n){
-#       if(precision_matrix[i,j]==0){
-#         precision_matrix[i,j] = (precision_matrix[i,k]*precision_matrix[k,j])
-#       }
-#     }
-#   }
-# }                   # ??(n^3) temporale ??(n^2) spaziale
-# 
-# # Creo il grafo di correlazione
-# 
-# correlation_graph <- graph_from_adjacency_matrix(precision_matrix, 
-#                                                  mode = "directed",
-#                                                  weighted = TRUE,
-#                                                  )
-# 
-# #Stampa del grafo di correlazione
-# 
-# plot.igraph(correlation_graph, edge.label=round(E(correlation_graph)$weight, 3), vertex.size=10)
-# 
-# plot( correlation_graph, layout = layout.reingold.tilford,
-#       edge.width = 1,
-#       edge.arrow.width = 0.3,
-#       vertex.size = 5,
-#       edge.arrow.size = 0.5,
-#       vertex.size2 = 3,
-#       vertex.label.cex = 1,
-#       asp = 0.35,
-#       margin = -0.1,
-#       edge.label=round(E(correlation_graph)$weight, 3),
-#       vertex.label=NA)
-
+#Calcolo la matrice differenza tra la matrice di adiacenza della METAPATHWAY e tra la matrice di adiacenza del grafo di correlazione
+diffmatrix <- adj_metapathway - precision_matrix
